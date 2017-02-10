7bf1337549cdd9742e578b7cfb2bcd8021def5bb
Olivier Debeir <odebeir@ulb.ac.be>
2017-02-10 11:38:05 +0100

    add pandas test

--- assignement1.py
+++ assignement1.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 
 # content of test_sample.py
 def inc(x):
@@ -10,3 +10,10 @@ def test_answer():
 
 def test_np():
     assert np.eye(3).sum() ==3.0
+
+def test_pandas():
+    df = pd.read_csv('MOCK_DATA.csv')
+    assert len(df) == 1000
+
+if __name__ == '__main__':
+    test_pandas()
