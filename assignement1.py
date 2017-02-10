eeb5a07df1c96d225998c878eb7c57b64d696530
Olivier Debeir <odebeir@ulb.ac.be>
2017-02-10 08:35:06 +0100

    add requirements

--- assignement1.py
+++ assignement1.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 # content of test_sample.py
 def inc(x):
     return x + 1
@@ -5,3 +8,5 @@ def inc(x):
 def test_answer():
     assert inc(4) == 5
 
+def test_np():
+    assert np.eye(3).sum() ==3.0
